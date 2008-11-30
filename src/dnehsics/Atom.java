8c30e1c6ca5879e4f1b5d0b3ea699191fcb3136f
Henry Bradlow <henrybrad@gmail.com>
2008-11-30 15:31:21 -0800

    changes to atom

--- src/dnehsics/Atom.java
+++ src/dnehsics/Atom.java
@@ -7,6 +7,7 @@ package dnehsics;
 
 import java.util.LinkedList;
 import javax.media.j3d.BranchGroup;
+import javax.vecmath.Color3f;
 import javax.vecmath.Vector3f;
 
 /**
@@ -18,17 +19,16 @@ import javax.vecmath.Vector3f;
 public class Atom extends Body{
     protected int numElectrons;
     protected int numProtons;
-    public Atom(Vector3f p, Vector3f v, float r, int nume, int nump, BranchGroup master, LinkedList<Body> bodyList)
+    public Atom(Vector3f p, Vector3f v, float r, int nume, int nump, float m,Color3f color, BranchGroup master, LinkedList<Body> bodyList)
     {
-        super(p,v,r,(float)(nump*Body.PROTON_CHARGE+nume*Body.ELECTRON_CHARGE),master,bodyList);
+        super(p,v,r,(float)(nump*Body.PROTON_CHARGE+nume*Body.ELECTRON_CHARGE),m,color,master,bodyList);
         numElectrons = nume;
         numProtons = nump;
-        System.out.println(getCharge(100));
     }
     public float getCharge(double d)
     {
         d = d*15.0;
-        double eFraction = 1-Math.pow(Math.E, -(d*d));
+        double eFraction = 1-Math.pow(Math.E, -(Math.pow(d,4)));
         return (float) (numProtons*Body.PROTON_CHARGE + numElectrons*eFraction*Body.ELECTRON_CHARGE);
     }
 }
